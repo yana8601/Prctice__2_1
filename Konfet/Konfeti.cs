7e6cd22903ea19a0e5f22232b2d3c0f2e79f8c0a
yana8601 <kovrigina8601@ukr.net>
2020-11-29 14:56:42 +0200

    create any classes

---
+++ Konfet/Konfeti.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konfet
+{
+    public class Konfeti
+    {
+
+
+        public static void SweetSimple(string name,float mass,float sugarPercent)
+        { 
+        
+
+        }
+
+    }
+}
--- Prctice__2_1/Program.cs
+++ Prctice__2_1/Program.cs
@@ -10,48 +10,79 @@ namespace Prctice__2_1
     {
         static void Main(string[] args)
         {
+            Sweets a = new Sweets("pchelka", 10f, 3f);
 
+            Sweets b = new Sweets("barbaris", 8f, 2.5f);
+
+            Chokolates c = new Chokolates("mishka", 15f, 5f, 0.52f, true, 10);
+
+            ChokolatesWithFilling d = new ChokolatesWithFilling("So vcusom koniaca", 15f, 3f, 0.75f, true, 10, 8);
+
+            ChokolatesWithFilling e = new ChokolatesWithFilling("Vechirniy kiyv", 20f, 4f, 0.52f, true, 8, 10);
+
+           
+
+
+
+            Console.ReadKey();
         }
     }
 
-    class Sweets
+   public class Sweets
     {
         public string name;
         public float mass;
         public float sugarPercent;
-    
+
+        public Sweets(string name, float mass, float sugar)
+        {
+            Console.WriteLine($"sweet {name}");
+            Console.WriteLine();
+        }
+
     }
 
-    class Chokolates : Sweets
+    public class Chokolates : Sweets
     {
         public float cacaoPercent;
         public bool isPakcedByBox;
         public int maxNomberInBox;
-        public float boxMass;
+        public float boxMass = 0;
 
-        public void Package()
+
+        public Chokolates(string name, float mass, float sugar, float cacaoPercent, bool isPakcedByBox, int maxNomberInBox)
+            : base(name, mass, sugar) 
         {
-      
             if (isPakcedByBox)
             {
                 boxMass = mass * maxNomberInBox;
-                Console.WriteLine($"Box of this chokolates has mass near {boxMass}");
+
+                Console.WriteLine($"Box of {name} chokolates has mass near {boxMass}g");
             }
+            else
+            {
+                Console.Write($"sweet {name} without boxes");
+            }
+
+            Console.WriteLine();
         }
+       
+       
     }
 
-
-    enum Filling { Berries, Jelly,Jam,WhiteChoko }
-    class ChokolatesWithFilling : Chokolates
+    public class ChokolatesWithFilling : Chokolates
     {
         public float fillingMass;
-        public Filling filling;
-        public void CheckMass()
+   
+        public ChokolatesWithFilling(string name, float mass, float sugar, float cacaoPercent, bool isPakcedByBox, int maxNomberInBox, float fillingMass)
+            : base(name, mass, sugar, cacaoPercent, isPakcedByBox, maxNomberInBox)
         {
             if (fillingMass >= mass)
             {
-                fillingMass = mass*0.8f;
+                fillingMass = mass * 0.8f;
             }
+
         }
+
     }
 }
